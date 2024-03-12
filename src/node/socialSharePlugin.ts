8fe2f40b73bd055c41de1e4637e1cc7c74fc8b78
ntnyq <ntnyq13@gmail.com>
2024-03-12 17:35:48 +0800

    feat: add debug logger

--- src/node/socialSharePlugin.ts
+++ src/node/socialSharePlugin.ts
@@ -1,32 +1,49 @@
-import { URL, fileURLToPath } from 'node:url'
-import { resolve } from 'node:path'
+import { getDirname, path } from 'vuepress/utils'
+import { addViteSsrNoExternal } from '@vuepress/helper'
+import { PLUGIN_NAME, logger } from './logger.js'
 import { mergeNetworksData } from './networks.js'
-import type { Plugin } from '@vuepress/core'
+import type { PluginFunction } from 'vuepress/core'
 import type { SocialSharePluginOptions } from '../shared/index.js'
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const __dirname = getDirname(import.meta.url)
 
-export const socialSharePlugin = (options: SocialSharePluginOptions = {}): Plugin => {
-  const { componentName = 'SocialShare', useCustomStyle = false, ...restOptions } = options
-  return {
-    name: 'social-share',
+export const socialSharePlugin =
+  (options: SocialSharePluginOptions = {}): PluginFunction =>
+  app => {
+    const {
+      componentName = 'SocialShare',
+      useCustomStyle = false,
+      // Options to client
+      ...restOptions
+    } = options
 
-    clientConfigFile: resolve(__dirname, '../client/config.js'),
+    if (app.env.isDebug) {
+      logger.info('Options:', options)
+    }
 
-    alias: app => ({
-      '@vuepress/plugin-social-share/options': app.dir.temp('social-share/options'),
-    }),
+    return {
+      name: PLUGIN_NAME,
 
-    define: {
-      __SOCIAL_SHARE_COMPONENT_NAME__: componentName,
-      __SOCIAL_SHARE_USE_CUSTOM_STYLE__: useCustomStyle,
-    },
+      clientConfigFile: path.resolve(__dirname, '../client/config.js'),
 
-    onPrepared(app) {
-      const networksData = mergeNetworksData(restOptions)
-      const socialShareOptions = { ...restOptions, networksData }
-      const content = `export const socialShareOptions = ${JSON.stringify(socialShareOptions)}`
-      app.writeTemp('social-share/options.js', content)
-    },
+      alias: app => ({
+        '@vuepress/plugin-social-share/options': app.dir.temp('social-share/options'),
+      }),
+
+      define: {
+        __SOCIAL_SHARE_COMPONENT_NAME__: componentName,
+        __SOCIAL_SHARE_USE_CUSTOM_STYLE__: useCustomStyle,
+      },
+
+      onPrepared(app) {
+        const networksData = mergeNetworksData(restOptions)
+        const socialShareOptions = { ...restOptions, networksData }
+        const content = `export const socialShareOptions = ${JSON.stringify(socialShareOptions)}`
+        app.writeTemp('social-share/options.js', content)
+      },
+
+      extendsBundlerOptions: (bundlerOptions: unknown, app): void => {
+        addViteSsrNoExternal(bundlerOptions, app, '@vuepress/helper')
+      },
+    }
   }
-}
