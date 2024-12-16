6ea4fb00ed6cbc0ff8b180fc234399b2fb7c33f1
ntnyq <ntnyq13@gmail.com>
2024-12-17 04:44:50 +0800

    fix: fix sharer `x` color when dark mode

--- src/node/networks.ts
+++ src/node/networks.ts
@@ -92,7 +92,10 @@ export const BUILT_IN_NETWORKS: SocialShareNetworkData = {
   x: {
     sharer: 'https://x.com/intent/tweet?text=@title&url=@url&hashtags=@hashtags@twitteruser',
     type: 'popup',
-    color: '#0f1419',
+    color: {
+      light: '#0f1419',
+      dark: '#fff',
+    },
     icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2L487 464H345L233.7 318.6L106.5 464H35.8l164.9-188.5L26.8 48h145.6l100.5 132.9zm-24.8 373.8h39.1L151.1 88h-42z"/></svg>',
   },
 }
